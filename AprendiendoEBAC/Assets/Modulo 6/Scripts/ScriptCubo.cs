9717cc96a56b6594c9271894f2f8e1a843a58aac
Chesire Saxofone <josefeospina2014@gmail.com>
2024-12-09 12:41:12 -0500

    Tarea Modulo 7
    Hago entrega de la tarea del uso de condicionales y booleanos en Unity

--- AprendiendoEBAC/Assets/Modulo 6/Scripts/ScriptCubo.cs
+++ AprendiendoEBAC/Assets/Modulo 6/Scripts/ScriptCubo.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class ScriptCubo : MonoBehaviour
 {
-
+    public Scriptesfera scriptesfera;
+    public ScriptCapsula scriptcapsula;
+    public bool cambioColor3;
     private void Awake()
     {
-        var meshrendermaterial = GetComponent<MeshRenderer>().material;
-        Color c = new Color(Random.value, Random.value,Random.value);
-        meshrendermaterial.color = c;
+        //var meshrendermaterial = GetComponent<MeshRenderer>().material;
+        //Color c = new Color(Random.value, Random.value,Random.value);
+        //meshrendermaterial.color = c;
     }
     // Start is called before the first frame update
     void Start()
@@ -21,5 +23,22 @@ public class ScriptCubo : MonoBehaviour
     void Update()
     {
         
+    }
+    private void FixedUpdate()
+    {
+        if (scriptesfera.CambioColor == true && scriptcapsula.CambioColor2 == true)
+        {
+            cambioColor3 = false;
+            var meshrendermaterial = GetComponent<MeshRenderer>().material;
+            Color c = Color.white;
+            meshrendermaterial.color = c;
+        }
+        else
+        {
+            cambioColor3 = true;
+            var meshrendermaterial = GetComponent<MeshRenderer>().material;
+            Color c = Color.black;
+            meshrendermaterial.color = c;
+        }
     }
 }
